7f0dea6476419c737b076f42452f0558ec765f90
GomaG444 <GomaG444>
2025-06-19 16:29:28 -0500

    added the key

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# keys were here and removed because I was unable to push with the keys
+# there was a key here
 
 # Home route
 @app.route('/')
