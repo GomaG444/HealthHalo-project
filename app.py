e38ab172565fc75030242cd4b89c8684dc402d4c
GomaG444 <GomaG444>
2025-07-10 16:08:00 -0500

    changed app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def dashboard():
+    return render_template('index.html')
+
+
+@app.route('/chatbot')
+def chatbot():
+    return render_template('chatbot.html')
+if __name__ == '__main__':
+    app.run(debug=True)
